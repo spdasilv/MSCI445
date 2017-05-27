0c5cea43ca35e8e1f818bf23a8bc35e01c8b8f8c
Shiv-S <shivam.shar09@gmail.com>
2017-05-26 20:14:37 -0400

    Minor Changes
    Comments, field null check and password masking.

--- MSCI 445 - Lab 2/EmailForm.cs
+++ MSCI 445 - Lab 2/EmailForm.cs
@@ -24,23 +24,38 @@ namespace MSCI_445___Lab_2
 
         }
 
+        // Defines the action of the send button; once clicked
         private void Send_Click(object sender, EventArgs e)
         {
+            // Checks if the 'To' and 'From' fields are filled out or not
             if (!string.IsNullOrWhiteSpace(To.Text) || !string.IsNullOrWhiteSpace(From.Text))
             {
+                // Checks if the 'To' and 'From' contain valid email addresses as defined in the rubric
                 if (To.Text.Contains("@") && From.Text.Contains("@gmail.com"))
                 {
-                    Password pw = new Password(From.Text, To.Text, Subject.Text, Body.Text);
-                    pw.Show();
+                    // Checks if the 'Subject' and 'Body' contain fields are filled out or not
+                    if (!string.IsNullOrWhiteSpace(Subject.Text) && !string.IsNullOrWhiteSpace(Body.Text))
+                    {
+                        Password pw = new Password(From.Text, To.Text, Subject.Text, Body.Text);
+                        // Displays the password input form
+                        pw.Show();
+                    }
+                    else
+                    {
+                        // Prompts the user to fill out the 'Subject' and 'Body' fields
+                        MessageBox.Show("Please make sure 'Subject' and 'Body' fields are filled.");
+                    }
                 }
                 else
                 {
+                    //Prompts the user to use valid email addresses
                     MessageBox.Show("Please use valid email addresses");
                 }
             }
             else
             {
-                MessageBox.Show("Please make sure the 'To' and 'From' field are filled");
+                // Prompts the user to fill out the 'To' and 'From fields
+                MessageBox.Show("Please make sure the 'To' and 'From' fields are filled.");
             }
         }
     }
