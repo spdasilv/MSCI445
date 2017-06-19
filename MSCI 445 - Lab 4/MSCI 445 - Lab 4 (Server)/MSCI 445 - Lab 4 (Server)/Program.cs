940a1a606d8b795ad40121c95f3dd61f772cf9cd
spdasilv <stefannopolo@yahoo.com.br>
2017-06-19 19:58:26 -0400

    Add Lab 4

---
+++ MSCI 445 - Lab 4/MSCI 445 - Lab 4 (Server)/MSCI 445 - Lab 4 (Server)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace MSCI_445___Lab_4
+{
+    class Program
+    {
+        private const double LOSS_RATE = 0.3;
+        private const double AVG_DELAY = 100; //milliseconds
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Required arguments: port");
+                return;
+            }
+            int port = Convert.ToInt32(args[0]);
+            IPEndPoint localpt = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            // Create random number generator for use in simulating
+            // packet loss and network delay.
+            Random random = new Random();
+            // Create a datagram socket for receiving and sending UDP packets
+            // through the port specified on the command line.
+            UdpClient socket = new UdpClient();
+            socket.Client.SetSocketOption(SocketOptionLevel.Socket,
+            SocketOptionName.ReuseAddress, true);
+            socket.Client.Bind(localpt);
+            System.Net.IPEndPoint ep = null;
+            // Processing loop.
+            while (true)
+            {
+                // Block until the host receives a UDP packet.
+                byte[] rdata = socket.Receive(ref ep);
+                Console.WriteLine("Received UDP data");
+                // Print the recieved data.
+                string name_string = Encoding.ASCII.GetString(rdata);
+                Console.WriteLine("Just received: " + name_string);
+
+                // Decide whether to reply, or simulate packet loss.
+                if (random.NextDouble() < LOSS_RATE)
+                {
+                    Console.WriteLine(" Reply not sent.");
+                    continue;
+                }
+                // Simulate network delay.
+                Thread.Sleep((int)(random.NextDouble() * 2 * AVG_DELAY));
+                // Send reply.
+                socket.Send(rdata, rdata.Length, ep);
+                Console.WriteLine(" Reply sent.");
+            }
+        }
+    }
+}
